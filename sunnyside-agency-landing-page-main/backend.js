b4c86332fc341aa6c5f4c2da70f7969ba8362e6f
Gregório Nagata <79941974+GregorioNagataDoria@users.noreply.github.com>
2021-06-26 11:19:46 -0300

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/backend.js
@@ -0,0 +1,20 @@
+function hamburgerclicked(){
+
+if( document.getElementById('mobile-info').style.display == 'none') {
+
+    document.getElementById('mobile-info').style.display = 'block'
+    
+
+}
+
+else{
+
+    console.log('my balls')
+
+    document.getElementById('mobile-info').style.display = 'none'
+
+  
+}
+
+
+}
